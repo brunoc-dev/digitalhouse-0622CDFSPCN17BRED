1d6832241768ec8def2ff3fbc1a0fed01ee22551
laisribeiro <lais.ribeiro@paipe.co>
2022-11-28 18:48:35 -0300

    Add imgs serviços

--- modulo 4 - Express/Aula 25 - Vistas EJS/petShop/src/models/ServicoModel.js
+++ modulo 4 - Express/Aula 25 - Vistas EJS/petShop/src/models/ServicoModel.js
@@ -5,19 +5,22 @@ module.exports = {
         id: 1,
         nome: 'Banho',
         valor: 'R$ 30,00',
-        descricao: 'Higienização completa'
+        descricao: 'Higienização completa',
+        imagem: 'banho'
       },
       {
         id: 2,
         nome: 'Tosa',
         valor: 'R$ 15,00',
-        descricao: 'Tosa completa'
+        descricao: 'Tosa completa',
+        imagem: 'tosa'
       },
       {
         id: 3,
         nome: 'Corte de unhas',
         valor: 'R$ 5,00',
-        descricao: 'Corte de unhas, todas as patas'
+        descricao: 'Corte de unhas, todas as patas',
+        imagem: 'corte-unha'
       },
     ]
   }
