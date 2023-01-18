736d08cf9b4d107273070fda7770abfc3787e27a
Bruno Razera <brunocrazera@gmail.com>
2023-01-18 20:37:51 -0300

    fix folder name

--- modulo 4 - Express/Aula 31 - Sessions & Cookies/petshop/src/controllers/BaseController.js
+++
@@ -1,8 +0,0 @@
-const ServicoModel = require('../models/ServicoModel');
-
-module.exports = {
-  index: (req, res) => {
-    const servicos = ServicoModel.index();
-    return res.render('index', { servicos });
-  }
-}
