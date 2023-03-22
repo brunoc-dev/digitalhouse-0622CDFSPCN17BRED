0455ca7b01a3d20737e6427cda80b9b100b731c5
Bruno Razera <brunocrazera@gmail.com>
2023-03-22 19:58:58 -0300

    Aula 43

---
+++ modulo 7 - React/Aula 42 - Introdução ao React/dh-school/src/components/Sidebar/index.jsx
@@ -0,0 +1,44 @@
+function Sidebar () {
+  return (
+    <aside className="sidemenu">
+      <ul className="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <li className="nav-item">
+          <a className="nav-link" href="/">
+            <i className="fas fa-fw fa-tachometer-alt"></i>
+            <span>
+              Dashboard - DH movies
+            </span>
+          </a>
+        </li>
+        <hr className="sidebar-divider" />
+        <div className="sidebar-heading">
+          Actions
+        </div>
+        <li className="nav-item">
+          <a className="nav-link" href="/">
+          <i className="fas fa-fw fa-gears"></i>
+            <span>
+              Genres
+            </span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">
+            <i className="fas fa-fw fa-film"></i>
+            <span>
+              Movies
+            </span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">
+            <i className="fas fa-fw fa-user"></i>
+            <span>Actors</span>
+          </a>
+        </li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
---
+++ modulo 7 - React/Aula 43 - Componentes React/dh-school/src/components/ContentWrapper.js
@@ -0,0 +1,27 @@
+import ContentRowTop from "./ContentRowTop";
+import { Footer } from "./Footer";
+import { TopBar } from "./TopBar";
+
+// variaveis no js seguimos o padrão camelCase, que seria a primeira letra minúscula e qualquer espaço que tenha, separamos pela próxima letra maiúscula.
+
+export function ContentWrapper(){
+  return(
+    <div id="content-wrapper" className="d-flex flex-column">
+
+			{/* <!-- Main Content --> */}
+			<div id="content">
+
+				{/* <!-- Topbar --> */}
+				{/* <!-- End of Topbar --> */}
+				{/* <!-- Content Row Top --> */}
+				{/* <!--End Content Row Top--> */}
+			</div>
+			{/* <!-- End of MainContent --> */}
+
+			{/* <!-- Footer --> */}
+			{/* <!-- End of Footer --> */}
+
+      {/* <!-- End of Content Wrapper --> */}
+		</div>
+  )
+}
