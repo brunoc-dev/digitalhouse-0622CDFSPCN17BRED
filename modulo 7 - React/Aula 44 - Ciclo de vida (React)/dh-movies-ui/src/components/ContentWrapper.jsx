332e06dd5052d0a7cf21ed0f8f21da8656685cd2
Bruno Razera <brunocrazera@gmail.com>
2023-03-27 19:55:00 -0300

    aula 44

---
+++ modulo 7 - React/Aula 44 - Ciclo de vida (React)/dh-movies-ui/src/components/ContentWrapper.jsx
@@ -0,0 +1,29 @@
+import ContentRowTop from "./ContentRowTop";
+import Footer from "./Footer";
+import TopBar from "./TopBar";
+
+// variaveis no js seguimos o padrão camelCase, que seria a primeira letra minúscula e qualquer espaço que tenha, separamos pela próxima letra maiúscula.
+
+export function ContentWrapper(){
+  return(
+    <div id="content-wrapper" className="d-flex flex-column">
+
+			{/* <!-- Main Content --> */}
+			<div id="content">
+
+				{/* <!-- Topbar --> */}
+				<TopBar />
+				{/* <!-- End of Topbar --> */}
+				{/* <!-- Content Row Top --> */}
+				<ContentRowTop />
+				{/* <!--End Content Row Top--> */}
+			</div>
+			{/* <!-- End of MainContent --> */}
+
+			{/* <!-- Footer --> */}
+			<Footer />
+			{/* <!-- End of Footer --> */}
+      {/* <!-- End of Content Wrapper --> */}
+		</div>
+  )
+}
